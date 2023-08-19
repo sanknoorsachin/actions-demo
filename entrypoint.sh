54b92337b9ef5231845a2e1c7019061cc2d7a9b7
sanknoorsachin <sachinsanknoor@gmail.com>
2023-08-19 20:35:01 +0530

    issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
 ls -ltr $GITHUB_WORKSPACE
+ls -ltr $ {{ github.workspace }}
+ls -ltr $$RUNNER_WORKSPACE
+ls -ltr $ {{ runner.workspace }}
 
 time= $(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+
